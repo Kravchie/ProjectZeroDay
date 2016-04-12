107bcee603af74ac650331ddbf38b7463b52e4ca
Klaudia Krawiecka <krawiecka.kl@gmail.com>
2016-04-12 17:42:11 +0200

    Changed ZeroDayGame and Menu view

--- core/src/com/crazysquirrelsofdestruction/zeroday/ZeroDayGame.java
+++ core/src/com/crazysquirrelsofdestruction/zeroday/ZeroDayGame.java
@@ -5,25 +5,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.crazysquirrelsofdestruction.zeroday.view.Menu;
 
 public class ZeroDayGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("table.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.end();
-
-
+		Menu menu = new Menu();
+		menu.render();
 	}
 }
