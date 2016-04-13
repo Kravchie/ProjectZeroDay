e9733d88f6587a168b27a20474a54c06891f80ab
Klaudia Krawiecka <krawiecka.kl@gmail.com>
2016-04-13 15:33:18 +0200

    Change name of menu file, change settings

--- core/src/com/crazysquirrelsofdestruction/zeroday/view/Menu.java
+++ core/src/com/crazysquirrelsofdestruction/zeroday/view/Menu.java
@@ -11,18 +11,18 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Menu {
 
     SpriteBatch batch;
-    Texture img;
+    Texture background;
 
     public Menu() {
         batch = new SpriteBatch();
-        img = new Texture("table.jpg");
+        background = new Texture("menu.jpg");
     }
 
     public void render () {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(img, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         batch.end();
     }
 }
--- core/src/com/crazysquirrelsofdestruction/zeroday/view/Settings.java
+++ core/src/com/crazysquirrelsofdestruction/zeroday/view/Settings.java
@@ -4,4 +4,6 @@ package com.crazysquirrelsofdestruction.zeroday.view;
  * Created by Klaudia on 2016-04-12.
  */
 public class Settings {
+
+
 }
